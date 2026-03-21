28faf2911461e68c004b66054843f81c99fbd8c3
Vikas Pandey <vikaspandey@Vikass-MacBook-Air.local>
2026-03-21 23:32:04 +0530

    Added tickets page

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -4,27 +4,23 @@ import { useEffect, useState } from "react";
 export default function TicketsPage() {
   const [tickets, setTickets] = useState([]);
 
-  const fetchTickets = async () => {
-    const res = await fetch("http://127.0.0.1:8000/tickets");
-    const data = await res.json();
-    setTickets(data);
-  };
-
   useEffect(() => {
-    fetchTickets();
+    fetch("https://ticketing-backend-i02l.onrender.com")
+      .then((res) => res.json())
+      .then((data) => setTickets(data));
   }, []);
 
   return (
     <main className="p-10">
       <h1 className="text-2xl font-bold mb-4">All Tickets</h1>
 
-      <ul>
-        {tickets.map((t: any, index) => (
-          <li key={index} className="border p-2 mb-2">
-            <strong>{t.title}</strong> - {t.description} ({t.email})
-          </li>
-        ))}
-      </ul>
+      {tickets.map((t: any) => (
+        <div key={t.id} className="border p-4 mb-2 rounded">
+          <h2 className="font-semibold">{t.title}</h2>
+          <p>{t.description}</p>
+          <p className="text-sm text-gray-500">{t.email}</p>
+        </div>
+      ))}
     </main>
   );
 }
